2125839f5e1b539b91f54a08fb580a830b4712ba
v-hajia <jianghm.ustc@gmail.com>
2019-12-01 16:25:17 +0000

    support attachment

--- flashtool/emailutils.py
+++ flashtool/emailutils.py
@@ -1,7 +1,11 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.utils import COMMASPACE, formatdate
 import json
 import os
+from os.path import basename
 
 
 def checkemail_exists(email):
@@ -24,12 +28,13 @@ def setup_email(email):
         json.dump(pwDict, file)
 
 
-def send_email(port = 587,password = None, 
+def send_email(port = 587,password = None,
         sender_email = "gtflashauto@gmail.com",
         smtp_server="smtp.gmail.com",
         receiver_email = None,
         subject="Subject",
-        message="Hello! "):
+        message="Hello! ",
+        attachment=None):
     """
     import flashtool.email as e
     e.send_email()
@@ -54,10 +59,24 @@ def send_email(port = 587,password = None,
     Best,
     GT ISYE FLASH ML GROUP
     """.format(message)
-    msg = MIMEText(body)
+    msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender_email
     msg['To'] = receiver_email
+    msg['Date'] = formatdate(localtime=True)
+    msg.attach(MIMEText(body))
+
+    if attachment is not None:
+        if not isinstance(attachment,list):
+            attachment = [attachment]
+        for f in attachment:
+            with open(f,'rb') as fil:
+                part = MIMEApplication(
+                        fil.read(),
+                        Name=basename(f)
+                        )
+            part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+            msg.attach(part)
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
